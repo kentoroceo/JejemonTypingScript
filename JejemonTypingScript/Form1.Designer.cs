0c6b0e2a5a9fad513d0f797b81c0fd1f019c23bd
Kent Oroceo <kjoroceo@yahoo.com>
2021-02-17 05:21:02 +0800

    Add project files.

---
+++ JejemonTypingScript/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace JejemonTypingScript
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.EnableCheckBox = new MaterialSkin.Controls.MaterialCheckBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // EnableCheckBox
+            // 
+            this.EnableCheckBox.AutoSize = true;
+            this.EnableCheckBox.Depth = 0;
+            this.EnableCheckBox.Font = new System.Drawing.Font("Roboto", 10F);
+            this.EnableCheckBox.Location = new System.Drawing.Point(197, 148);
+            this.EnableCheckBox.Margin = new System.Windows.Forms.Padding(0);
+            this.EnableCheckBox.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.EnableCheckBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.EnableCheckBox.Name = "EnableCheckBox";
+            this.EnableCheckBox.Ripple = true;
+            this.EnableCheckBox.Size = new System.Drawing.Size(71, 30);
+            this.EnableCheckBox.TabIndex = 1;
+            this.EnableCheckBox.Text = "Enable";
+            this.EnableCheckBox.UseVisualStyleBackColor = true;
+            this.EnableCheckBox.CheckedChanged += new System.EventHandler(this.EnableCheckBox_CheckedChanged);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(74, 245);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(329, 19);
+            this.materialLabel1.TabIndex = 2;
+            this.materialLabel1.Text = "Note: This will change your keystrokes globally.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 342);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.EnableCheckBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jejemon Typing Script";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialCheckBox EnableCheckBox;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+    }
+}
+
---
+++ JejemonTypingScript/Form1.cs
@@ -0,0 +1,88 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JejemonTypingScript
+{
+    public partial class Form1 : Form
+    {
+        List<KeyboardHook> hooks = new List<KeyboardHook>();
+        List<Boolean> registered = new List<Boolean>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                KeyboardManager.PressKey(Keys.CapsLock);
+
+                switch (m.WParam.ToInt32())
+                {
+                    case (int)Keys.A:
+                        KeyboardManager.PressKey(Keys.D4); //4
+                        KeyboardManager.PressKey(Keys.H); //4
+                        break;
+                    case (int)Keys.E:
+                        KeyboardManager.PressKey(Keys.D3); //3
+                        break;
+                    case (int)(Keys.O):
+                        KeyboardManager.PressKey(Keys.D0); //0
+                        KeyboardManager.PressKey(Keys.W); //W
+                        break;
+                    case (int)(Keys.S):
+                        KeyboardManager.PressKey(Keys.Z); //Z
+                        KeyboardManager.PressKey(Keys.CapsLock);
+                        KeyboardManager.PressKey(Keys.Z); //Z
+                        break;
+
+                }
+                
+            }
+            base.WndProc(ref m);
+        }
+        private void EnableCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (EnableCheckBox.Checked)
+            {
+                RegisterKeys();
+            }
+            else
+            {
+                UnregisterKeys();
+            }
+        }
+
+        private void RegisterKeys()
+        {
+            hooks.Add(new KeyboardHook((int)KeyboardHook.Modifiers.None, (int)Keys.A, Keys.A, this)); //A
+            hooks.Add(new KeyboardHook((int)KeyboardHook.Modifiers.None, (int)Keys.E, Keys.E, this)); //E
+            hooks.Add(new KeyboardHook((int)KeyboardHook.Modifiers.None, (int)Keys.O, Keys.O, this)); //O
+            hooks.Add(new KeyboardHook((int)KeyboardHook.Modifiers.None, (int)Keys.S, Keys.S, this)); //S
+            foreach (KeyboardHook hook in hooks)
+            {
+                hook.Register(); 
+            }
+        }
+
+        private void UnregisterKeys()
+        {
+            foreach (KeyboardHook hook in hooks)
+            {
+                hook.Unregister();
+            }
+        }
+    }
+}
